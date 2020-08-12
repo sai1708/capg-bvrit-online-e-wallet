09393f40cbf9129b98289c8de24570c89f7f4f2c
SOUMYA <SOUMYA@SRINIVAS>
2020-08-12 14:00:27 +0530

    Showbalance

---
+++ Online-ewallet-showbalance/src/main/java/com/capg/ewallet/OnlineEwalletShowbalanceApplication.java
@@ -0,0 +1,13 @@
+package com.capg.ewallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineEwalletShowbalanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineEwalletShowbalanceApplication.class, args);
+	}
+
+}
