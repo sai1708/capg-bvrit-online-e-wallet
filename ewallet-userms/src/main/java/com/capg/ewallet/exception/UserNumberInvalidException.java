453cb76ac26416fa0facdcfc8d63fe3f541fe9e7
SOUMYA <SOUMYA@SRINIVAS>
2020-08-20 18:22:40 +0530

    invaildexceptions added

---
+++ ewallet-userms/src/main/java/com/capg/ewallet/exception/UserNumberInvalidException.java
@@ -0,0 +1,8 @@
+package com.capg.ewallet.exception;
+
+public class UserNumberInvalidException extends RuntimeException {
+	public UserNumberInvalidException(String message)
+	{
+		super(message);
+	}
+}
